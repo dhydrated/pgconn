a24667cf265bf6a6b08124e744c3404c84fa6b6c
taufekj <taufek.johar@my.experian.com>
2014-10-27 23:26:34 +0800

    remove unnecessary file
    added dependecies

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pgconn',
-      version='0.1',
+      version='0.2',
       scripts=['bin/pgconn'],
       description='Utility to connect to your Postgres DB using your .pgpass file',
       url='http://github.com/dhydrated/pgconn',
@@ -9,4 +9,7 @@ setup(name='pgconn',
       author_email='dhydrated@gmail.com',
       license='MIT',
       packages=['pgconn'],
+      install_requires=[
+          'os', 'optparse', 'logging'
+      ],
       zip_safe=False)
